ec0a0c729b7fd18b81a8409719cf11d983dce1d6
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-10-23 21:07:56 -0500

    commit 1

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
+//Richard Padilla
 public class Empleado {
+
+    private String nombre;
+    private int edad;
+    private double salario;
+
+    public Empleado(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
+
+    public void Empleado1(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Edad: "+edad);
+        System.out.println("Salario: "+salario);
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Richard Padilla
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String nombre;
+        int edad;
+        double salario;
+
+        Scanner ingresar = new Scanner(System.in);
+
+        //Objetos con valores quemados
+        Empleado empleado1 = new Empleado("Richard", 27, 450.0);
+        System.out.println("--- Datos Quemados ---");
+        empleado1.Empleado1();
+
+        //Pedir datos atravez de Scanner
+        System.out.println("--- DATOS INGRESADOS ---");
+        System.out.println("Ingrese el nombre del empleado: ");
+        nombre = ingresar.nextLine();
+        System.out.println("Ingrese la edad del empleado: ");
+        edad = ingresar.nextInt();
+        System.out.println("Ingrese el salario del empleado: ");
+        salario = ingresar.nextDouble();
+
+        //nuevo objeto con los datos ingresados
+        Empleado empleado2 = new Empleado(nombre, edad, salario);
+        System.out.println("--- Datos Ingresados ---");
+        System.out.println("Nombre: " + empleado2.getNombre());
+        System.out.println("Edad: " + empleado2.getEdad());
+        System.out.println("Salario: " + empleado2.getSalario());
+
     }
+
 }
